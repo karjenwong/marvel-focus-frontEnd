34857895d9d5200271ed8220e592146f5b6e8775
Karjen Wong <karjenwong@gmail.com>
2019-03-27 10:02:38 -0700

    updated about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,12 @@ class About extends Component {
       <div>
         <h2>Who is Karjen Wong?</h2>
         <p>
-          I am a newly minted web developer gained after 12 weeks at
-          Brainstation. I started coding Jan 2019 with no background in code and
-          so far, I've enjoyed learning a lot of different programming stuff
-          like node.js, vanilla javascript, react and html/css.
+          I am a newly minted web developer after 12 weeks at Brainstation. I
+          started coding Jan 2019 with no background in code and so far enjoyed
+          learning a lot of different programming stuff like node.js, vanilla
+          javascript, react and html/css. Can't believe how much I've learned in
+          the small amount of time and truly truly understand that there is so
+          much more to learn out there!
         </p>
         <h3>Links below</h3>
         <a href="https://github.com/karjenwong?tab=repositories">
@@ -26,8 +28,8 @@ class About extends Component {
         <p>
           Simply put, I wanted design a website to help people learn more about
           the marvel universe. I tried my best to incorporate the offical marvel
-          api as much as possible in connection with marvel movies coming out
-          and upcoming movies.
+          api as much as possible and also tapping into the TheMovieDb api. And
+          why not watch a youtube trailer while you're here?
         </p>
       </div>
     );
