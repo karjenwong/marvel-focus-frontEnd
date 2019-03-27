3bd37ea1cfdd32dca4bc99d98d6e6c3a8f41f393
Karjen Wong <karjenwong@gmail.com>
2019-03-27 09:54:25 -0700

    updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,24 @@ class About extends Component {
   render() {
     return (
       <div>
+        <h2>Who is Karjen Wong?</h2>
+        <p>
+          I am a newly minted web developer gained after 12 weeks at
+          Brainstation. I started coding Jan 2019 with no background in code and
+          so far, I've enjoyed learning a lot of different programming stuff
+          like node.js, vanilla javascript, react and html/css.
+        </p>
+        <h3>Links below</h3>
+        <a href="https://github.com/karjenwong?tab=repositories">
+          <img className="logo" src="/Assets/githublogo.svg" alt="logo" />
+          Github
+        </a>
+
+        <a href="https://www.linkedin.com/in/karjenwong/">
+          <img className="logo" src="/Assets/Linkedinlogo.svg" alt="logo" />
+          LinkedIn
+        </a>
+
         <h2>What is this site about?</h2>
         <p>
           Simply put, I wanted design a website to help people learn more about
@@ -12,7 +30,6 @@ class About extends Component {
           and upcoming movies.
         </p>
       </div>
-      
     );
   }
 }
